0a479888dd910bca5eaea767c332d7a6c94febf8
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-17 23:51:21 +0500

    Task020 2.0

--- Task020/Program.cs
+++ Task020/Program.cs
@@ -1,16 +1,16 @@
-﻿Console.Write("Введите X Кординаты A: ");
-int X1 = int.Parse(Console.ReadLine()!);
+﻿Console.Write("Введите X1: ");
+double X1 = double.Parse(Console.ReadLine()!);
 
-Console.Write("Введите Y Кординаты A: ");
-int Y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y1: ");
+double Y1 = double.Parse(Console.ReadLine()!);
 
-Console.Write("Введите X Кординаты B: ");
-int X2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите X2: ");
+double X2 = double.Parse(Console.ReadLine()!);
 
-Console.Write("Введите Y Кординаты B: ");
-int Y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y2: ");
+double Y2 = double.Parse(Console.ReadLine()!);
 
 double d = Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2));
 
-Console.WriteLine($"d={d:f4}");
+Console.WriteLine($"d={d:f2}");
 
