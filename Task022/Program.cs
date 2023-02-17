07377273c2fe22c59456c4a1096cd7ff989f5fa9
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-17 23:21:12 +0500

    Task022 only positive numbers

---
+++ Task022/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int squares = int.Parse(Console.ReadLine()!);
+int index = 1;
+double sum = 0;
+
+while(index <= squares)
+{
+    sum = index * index;
+    Console.WriteLine(sum); 
+    index++;
+}
