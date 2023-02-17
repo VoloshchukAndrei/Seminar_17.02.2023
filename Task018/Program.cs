a2992992532c33f2983be0c165129a3fa33738a6
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-17 23:41:01 +0500

    Task018 switch

--- Task018/Program.cs
+++ Task018/Program.cs
@@ -1,16 +1,21 @@
 ﻿Console.Write("Введите номер четверти: ");
-int number = int.Parse(Console.ReadLine()!);
+int qurter = int.Parse(Console.ReadLine()!);
 
-if(number == 1)
+switch (qurter)
 {
-    Console.WriteLine("Ось X > 0 и Ось Y > 0");
-}   
-else if(number == 2){
-    Console.WriteLine("Ось X < 0 и Ось Y > 0");
-}else if(number == 3){
-    Console.WriteLine("Ось X < 0 и Ось Y < 0");
-}else if(number == 4){
-    Console.WriteLine("Ось X > 0 и Ось Y < 0");
-}else if(number > 4){
-    Console.WriteLine("Введён неверный номер четверти");
+    case 1:{Console.WriteLine("Ось X > 0 и Ось Y > 0");
+    break;
+    }
+    case 2:{Console.WriteLine("Ось X < 0 и Ось Y > 0");
+    break;
+    }
+    case 3:{Console.WriteLine("Ось X < 0 и Ось Y < 0");
+    break;
+    }
+    case 4:{Console.WriteLine("Ось X > 0 и Ось Y < 0");
+    break;
+    }
+    default:{Console.WriteLine("Введён неверный номер четверти");
+    break;
+    }
 }
